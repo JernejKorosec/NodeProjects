b4eac438ad4b25ddecdc7e3daaa22e56e57d3027
Jernej Korosec <jernejk.alfa@gmail.com>
2022-04-18 14:08:27 +0200

    Fist Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// Za delo z datotekami
+const fs = require('fs').promises;
+
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+// Za branje datotek
+// Naredi še check path da veš da zadeva deluje pravilno
+async function readFile(filePath) {
+  try {
+    const data = await fs.readFile(filePath);
+    console.log(data.toString());
+  } catch (error) {
+    console.error(`Got an error trying to read the file: ${error.message}`);
+  }
+}
+
+// Klicem funkcijo za pozdrav
+readFile('node-files/greetings.txt');
